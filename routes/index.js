36c5ab29dc4bb7fa2a1f87d55168bf31799aee02
Jessica <jessica.weeber@gmail.com>
2016-06-14 14:51:48 -0700

    completed /zomg route, json is successful

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).json({whatevs: 'whatevs!!!'})
-});
+})
 
-module.exports = router;
+router.get('/zomg', function(req, res) {
+  res.status(200).json({working: 'hell yeah it works!'})
+})
+
+module.exports = router
