3aac48f7cfb7f21510dda662d5fd547a1fb4f47d
Anna Wilson <annie.marie.mason@gmail.com>
2016-06-14 14:45:29 -0700

    tested zomg route and it worked

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.status(200).json({whatevs: 'whatevs!!!'})
 });
 
+router.get('/zomg', function(req, res, next) {
+  res.status(200).json({whatevs: 'it works!!!'})
+});
+
 module.exports = router;
