2eaf465494a08549c146a17c54614fe107f15e29
Suzanne Harrison <harrison_family@hotmail.com>
2016-06-14 14:55:31 -0700

    psuedo code for routes.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.status(200).json({whatevs: 'whatevs!!!'})
 });
 
+/*
+customers
+GET customers
+GET customers/sort/name?n=10&p=2
+GET customers/sort/registered_at
+GET customers/sort/postal_code
+
+GET customers/:id/current
+GET customers/:id/history
+
+movies
+GET movies
+GET movies/sort/release-date?n=5&p=1
+GET movies/sort/title
+GET movies/sort/release-date
+
+GET movies/:title/current
+GET movies/:title/history/sort/name
+GET movies/:title/history/sort/checkout-date
+
+rentals
+GET rentals/:title
+GET rentals/:title/customers
+POST rentals/:title/checkout
+POST rentals/:title/return
+GET rentals/overdue
+
+*/
+
+
 module.exports = router;
