44cf9d11cd0e15adc5600a71dadb819cdfd0882e
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-06-14 16:05:59 -0700

    Added instance of controller to customers routes

--- routes/customers.js
+++ routes/customers.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var Controller = require('../controllers/customers')
 
 /* GET home page. */
 router.get('/sort/', function (req, res, next) {
