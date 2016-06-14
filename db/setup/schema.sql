cd8b916b182c9312342e75106303f6f1a77b6822
Nadine Curry <nlcurry@gmail.com>
2016-06-14 15:22:29 -0700

    created rentals join table and customer table

--- db/setup/schema.sql
+++ db/setup/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS movies;
 CREATE TABLE movies(
-  id PRIMARY KEY,
+  id integer PRIMARY KEY,
   title text,
   overview text,
   release_date text,
@@ -8,3 +8,27 @@ CREATE TABLE movies(
 );
 
 CREATE INDEX movies_title ON movies (title);
+
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers(
+  id integer PRIMARY KEY,
+  name text,
+  registered_at text,
+  address text,
+  city text,
+  state text,
+  postal_code text,
+  phone text,
+  account_credit decimal
+);
+
+CREATE INDEX customers_name ON customers (name);
+
+DROP TABLE IF EXISTS rentals;
+CREATE TABLE rentals(
+  id integer PRIMARY KEY,
+  movie_id integer REFERENCES movies (id),
+  customer_id integer REFERENCES customers (id)
+);
+
+CREATE INDEX rentals ON rentals (customer_id);
