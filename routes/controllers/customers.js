82528b91cdca5ac6d20f8b601b42228f3934523f
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-06-14 16:20:32 -0700

    Idunnolol

---
+++ routes/controllers/customers.js
@@ -0,0 +1,23 @@
+var CustomerController = {
+//   index: function (req, res, next) {
+//     res.render('index', { title: 'Express' })
+//   },
+//
+//   doSomething: function (request, response) {
+//     var locals = {}
+//     locals.title = 'RANDOM PARTY GENERATOR'
+//     // pick rand number between 1-100
+//     locals.attendees = Math.floor(Math.random() * 100 + 1)
+//     // pick random day of week
+//     function sample (array) {
+//       return array[Math.floor(Math.random() * array.length)]
+//     }
+//
+//     locals.day = sample(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+//     // pick a costume theme for your party
+//     locals.theme = sample(['Toga', 'Halloween', 'Black Light', 'Jungle', 'Neon'])
+//     response.render('party', locals)
+//   }
+// }
+
+module.exports = CustomerController
