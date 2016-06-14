aeaad3c7aa5043e2106d8d9587470d9f7efe51dd
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-06-14 16:27:20 -0700

    movies routes to controller, includes rentals

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var Controller = require('../controllers/movies')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.status(200).json({whatevs: 'whatevs!!!'})
-});
+router.get('/sort/title', Controller.sortTitle)
+router.get('/sort/release', Controller.sortRelease)
 
+router.get('/:title/current', Controller.current)
+router.get('/:title/history', Controller.history)
+
+router.get('/rentals/:title', Controller.rentalsTitle)
+router.get('/rentals/:title/customers', Controller.rentalsCustomers)
+router.post('/rentals/:title/checkout', Controller.checkout)
+router.patch('/rentals/:title/return', Controller.return)
+router.get('/rentals/overdue', Controller.overdue)
 module.exports = router;
