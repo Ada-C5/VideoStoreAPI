c58a53fab9a4ad2721b7d7136bba4c5c5a381f3b
Leah <leahnpetersen@gmail.com>
2016-06-14 14:43:59 -0700

    create index route to server it works json data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+// var Controller = require('../controllers/scrabble')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).json({whatevs: 'whatevs!!!'})
+  res.status(200).json({index: 'index'})
+});
+
+router.get('/zomg', function(req, res, next) {
+  res.status(200).json({whatevs: 'it works!!!!'})
 });
 
 module.exports = router;
