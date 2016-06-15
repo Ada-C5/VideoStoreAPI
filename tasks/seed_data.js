108f85ec90975511e14fee23de7f167a9d25d1ac
sophia <sophiabaldonado@gmail.com>
2016-06-15 15:45:01 -0700

    clean up seeding task;

--- tasks/seed_data.js
+++ tasks/seed_data.js
@@ -12,45 +12,41 @@ var rentalsCount = 0
 
 function seed() {
   // saveSync is not asynchronous, it's blocking, ok for stuff like this that is run every once in a while
-  // while (seeded_customers !== customers_data.length && seeded_movies !== movies_data.length) {
-    for (var movie of movies_data) {
-      db.movies.save(movie, function (err,res) {
-        if (err) {
-          throw new Error(err.message)
-        }
-        moviesCount++
-        checkFinish()
-      })
-    }
+  for (var movie of movies_data) {
+    db.movies.save(movie, function (err,res) {
+      if (err) {
+        throw new Error(err.message)
+      }
+      moviesCount++
+      checkFinish()
+    })
+  }
 
-    for (var customer of customers_data) {
-      db.customers.save(customer, function (err,res) {
-        if (err) {
-          throw new Error(err.message)
-        }
-        customersCount++
-        checkFinish()
-      })
-    }
+  for (var customer of customers_data) {
+    db.customers.save(customer, function (err,res) {
+      if (err) {
+        throw new Error(err.message)
+      }
+      customersCount++
+      checkFinish()
+    })
+  }
 
-    for (var rental of rentals_data) {
-      db.rentals.save(rental, function (err,res) {
-        if (err) {
-          throw new Error(err.message)
-        }
-        rentalsCount++
-        checkFinish()
-      })
-    }
-  // }
+  for (var rental of rentals_data) {
+    db.rentals.save(rental, function (err,res) {
+      if (err) {
+        throw new Error(err.message)
+      }
+      rentalsCount++
+      checkFinish()
+    })
+  }
 }
 
 function checkFinish() {
   var totalCount = moviesCount + customersCount + rentalsCount
   var totalLength = movies_data.length + customers_data.length + rentals_data.length
 
-  // console.log("total count", totalCount)
-  // console.log(totalCount >= totalLength)
   if (totalCount >= totalLength) { process.exit() }
 }
 
