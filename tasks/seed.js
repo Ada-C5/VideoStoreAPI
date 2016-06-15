1a9d7adfe9c3350db6843a23a205c543dbcef174
Leah <leahnpetersen@gmail.com>
2016-06-15 15:32:02 -0700

    seed success! seed js file working

--- tasks/seed.js
+++ tasks/seed.js
@@ -4,21 +4,55 @@ var db = massive.connectSync({connectionString : connectionString})
 
 // produces array with each element is an object key:value pair
 var moviesParsedJSON = require('../db/seeds/movies.json');
+// var movieLength = moviesParsedJSON.length
 var customersParsedJSON = require('../db/seeds/customers.json');
+// var customerLength = customersParsedJSON.length
 
+
+
+// ***********synchronous method
 for (var movie of moviesParsedJSON) {
-	console.log(movie.title, )
+	console.log(movie)
 	// pass save
 	// db.video_store.saveSync({movie: movie.title.....})
-	db.video_store.saveSync(movie)
+	db.movies.saveSync(movie);
 }
 
 
 for (var customer of customersParsedJSON) {
-	console.log(customer.name, customer.registered_at,  )
+	// console.log(customer.name, customer.registered_at,  )
 	// db.video_store.saveSync({name: customer.name, customer.registered_at, })
-	db.video_store.saveSync(customer)
+	db.customers.saveSync(customer);
 }
 
 console.log("seeding done!")
 process.exit()
+
+
+
+
+// check length of db
+// function checkFinish(records) {
+// 	db.video_store.count(function(err, res) {
+// 		console.log("words in db: ", res)
+// 		// need to make sure both files are done
+// 		if (res >= records) { process.exit() }
+// 	})
+// }
+
+// // **********a-synchronous method
+// for (var movie of moviesParsedJSON) {
+// 	db.video_store.save(movie, function(err, res) {
+// 		console.log('saved: ', JSON.stringify(res))
+// 		checkFinish(movieLength)
+// 	})
+// }
+
+// for (var customer of customersParsedJSON) {
+// 	db.video_store.save(customer, function(err, res) {
+// 		console.log('saved: ', JSON.stringify(res))
+// 		checkFinish(customerLength)
+// 	})
+// }
+
+
