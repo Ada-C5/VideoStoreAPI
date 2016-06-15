c841893083273f50470f9f9237afd8619801a9cf
Suzanne Harrison <harrison_family@hotmail.com>
2016-06-14 21:41:44 -0700

    made changes to schema.sql, package.json, and made tasks/seed_data.js. Seed data now loads, and includes logic to take care of asynchronous seeding for customers and movies.

---
+++ tasks/load_schema.js
@@ -0,0 +1,14 @@
+var massive = require('massive')
+var connectionString = "postgres://localhost/video_store_api"
+
+var db = massive.connectSync({connectionString : connectionString})
+
+// setup below comes from folder name
+db.setup.schema([], function(err, res) {
+  if(err) {
+    throw(new Error(err.message))
+  }
+
+  console.log("yay schema!")
+  process.exit()
+})
