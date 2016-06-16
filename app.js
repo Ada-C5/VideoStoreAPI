e4ff73cd14b093aa217125d05efdb0720f43de0e
Jade Vance <jadenicolevance@gmail.com>
2016-06-16 16:53:29 -0700

    required massive and cleaned up errors

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var massive = require('massive');
 
-var routes = require('./routes/index');
 
 // var videoRoutes = require('./routes/index');
 // app.use('/video', videoRoutes);
 
 var app = module.exports = express();
+
+// database setup
+var connectionString = "postgres://localhost/extreme_video_express";
+var db = massive.connectSync({connectionString: connectionString});
+app.set('db', db);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+var routes = require('./routes/index');
 app.use('/', routes);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,11 +31,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-// database setup
-var connectionString = "postgres://localhost/extreme_video_express" + app.get('env');
-var db = massive.connectSync({connectionString: connectionString});
-app.set('db', db);
 
 
 // catch 404 and forward to error handler
