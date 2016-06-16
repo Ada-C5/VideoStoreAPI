375f1da71a8f527b223c3619c4c28490a112970b
Nadine Curry <nlcurry@gmail.com>
2016-06-16 14:15:06 -0700

    added index function

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,7 +1,19 @@
+var Customer = require("../models/customer")
+
 var CustomersController = {
-
-  function index() {
-
+  index: function(req, res, next) {
+    Customer.all(function(error, customers) {
+      if(error) {
+        var err = new Error("Error retrieving customer list:\n" + error.message);
+        err.status = 500;
+        next(err);
+      } else {
+        res.json(customers)
+        // res.render("accounts/index", {
+        //   accounts: accounts
+        // });
+      }
+    });
   },
 
   function sortBy() {
