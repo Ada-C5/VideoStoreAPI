7475951e77ab93dfc1642ef893c193b8a074c046
Alysia Brown <alysiacbrown@gmail.com>
2016-06-16 16:51:14 -0700

    EOD commit.

--- models/rentals_model.js
+++ models/rentals_model.js
@@ -1,23 +1,21 @@
 var app = require("../app");
 var db = app.get("db");
 
-var Rental = function(title) {
-  this.title = title
+var Rental = function(id) {
+  this.id = id
 };
 
-Rental.getCheckedOut = function(id, callback) {
-  db.rentals.findOne(this.title, function(error, rental) {
+Rental.getCheckedOut = function(movie_id, callback) {
+  // Not sure if this would return in an array or if we'd have to convert it to an array
+  var array = db.rentals.where("movie_id=$1 AND returned=$2", [movie_id, false], function(error, rental) {
     if(error) {
       callback(error, undefined);
     } else {
-      var checked_out = 0;
+      var checked_out = array.length;
       callback(null, checked_out);
     }
   })
 }
-
-
-
 
 
 
