3e69ff878b80baad45dae3989f2d5fab9d862076
Nadine Curry <nlcurry@gmail.com>
2016-06-16 15:32:58 -0700

    ignoring some line pt 2

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
-var MoviesController = require('../controllers/movies')
+// var express = require('express');
+// var router = express.Router();
+// var MoviesController = require('../controllers/movies')
 
-router.get('/', MoviesController.listMovies)
-router.get('/sort/:query', MoviesController.sortBy)
-router.get('/:movie/current', MoviesController.current)
-router.get('/:movie/history/sort/:by', MoviesController.sortedHistory)
+// router.get('/', MoviesController.listMovies)
+// router.get('/sort/:query', MoviesController.sortBy)
+// router.get('/:movie/current', MoviesController.current)
+// router.get('/:movie/history/sort/:by', MoviesController.sortedHistory)
 
 
-module.exports = router;
+// module.exports = router;
