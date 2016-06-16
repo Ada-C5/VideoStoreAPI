07215f65150168bfa980bf39ec2b2610c240d379
Nadine Curry <nlcurry@gmail.com>
2016-06-16 16:03:34 -0700

    made index function for movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,20 +1,29 @@
+var Movie = require("../models/movie")
+
 var MoviesController = {
-
-  function listMovies () {
-
-  },
-
-  function sortBy () {
-
-  },
-
-  function current () {
-
-  },
-
-  function sortedHistory () {
-
+  index: function(req, res, next) {
+    Movie.all(function(error, movies) {
+      if(error) {
+        var err = new Error("Error retrieving customer list:\n" + error.message);
+        err.status = 500;
+        next(err);
+      } else {
+        res.json(movies)
+      }
+    });
   }
+
+  // function sortBy () {
+
+  // },
+
+  // function current () {
+
+  // },
+
+  // function sortedHistory () {
+
+  // }
 }
 
-modules.exports = MoviesController
+module.exports = MoviesController
