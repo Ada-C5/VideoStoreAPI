9c4d9a7675ec7fdaf7db6b8f108a19885a043840
Suzanne Harrison <harrison_family@hotmail.com>
2016-06-16 16:28:48 -0700

    finished redoing the routes for rentals.

--- routes/rentals.js
+++ routes/rentals.js
@@ -2,10 +2,24 @@ var express = require('express');
 var router = express.Router();
 var RentalsController = require('../controllers/rentals_controller.js');
 
-/* GET customers listing. */
-router.get('/', RentalsController.index);
+// GET rentals/:title
+// Look a rental up by title
+router.get('/:title', RentalsController.findMovie)
 
-/* GET customers details. */
-/* router.get('/:id', RentalsController.show); */
+// GET rentals/:title/customers
+// list of customers that have currently checked out any of the movie's inventory
+router.get('/:title/customers', RentalsController.sortBy)
+
+// POST rentals/:title/check-out
+// #provide customerid and movie title
+router.post('/:title/check-out', RentalsController.checkout)
+
+// POST rentals/:title/return
+// #provide customerid and movie title
+router.post('/:title/return', RentalsController.return)
+
+// GET rentals/overdue
+// list of customers with overdue movies
+router.get('/overdue', RentalsController.overdue)
 
 module.exports = router;
