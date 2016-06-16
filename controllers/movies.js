74f7d14703487d864abe32b1337eafff8fafbc74
Valerie <val.conklin@gmail.com>
2016-06-16 16:33:47 -0700

    Set up error handling for movies controller index method

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,10 +4,14 @@ var Movie = require("../models/movie.js");
 var MoviesController = {
   index: function(req, res, next) {
     Movie.all(function (error, result){
+      if (error) {
+        var err = new Error("Error retrieving movies:\n" + error.message);
+        err.status = 500;
+        next(err);
+      } else {
+        res.json(result);
+      }
 
-      
-
-      return result;
     };
 
   }
