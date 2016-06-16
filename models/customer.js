b5ef86647dc58de68b6ffa75f3e5923ed16df5f1
Nadine Curry <nlcurry@gmail.com>
2016-06-16 15:33:21 -0700

    created sortbyname function

--- models/customer.js
+++ models/customer.js
@@ -2,8 +2,9 @@ var app = require("../app");
 var db = app.get("db");
 
 // Constructor function
-var Customer = function(id) {
-  this.id = id;
+var Customer = function(customer) {
+  this.id = customer.id;
+  this.name = customer.name;
 };
 
 
@@ -12,11 +13,22 @@ Customer.all = function(callback) {
     if(error || !customers) {
       callback(error || new Error("Could not retrieve Customers"), undefined);
     } else {
-      callback(null, customers.map(function(Customer) {
-        return new Customer(customers.id);
+      callback(null, customers.map(function(customer) {
+        return new Customer(customer);
       }));
     }
   });
 };
 
+Customer.sortByName = function(input, callback){
+  db.run("Select * From (Select Row_Number() Over (Order By name) As RowNum, *From customers) customers Where RowNum BETWEEN $1 AND $2;",input, function(error, customers) {
+    if(error || !customers) {
+      callback(error || new Error("Could not retrieve Customers"), undefined);
+    } else {
+      callback(null, customers.map(function(customer) {
+        return new Customer(customer);
+      }));
+    }
+  });
+};
 module.exports = Customer
