752355941a48d2f1adba34931a67f36fc56c63e6
Nadine Curry <nlcurry@gmail.com>
2016-06-16 15:32:47 -0700

    ignoring some lines

--- routes/customers.js
+++ routes/customers.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var CustomersController = require('../controllers/customers')
 
 router.get('/', CustomersController.index) // nothing here, probably omit
-router.get('/sort/:query', CustomersController.sortBy(params.req.query))
-router.get('/:id/current', CustomersController.current)
-router.get('/:id/history', CustomersController.history)
+router.get('/sort/:query', CustomersController.sortBy)
+// router.get('/:id/current', CustomersController.current)
+// router.get('/:id/history', CustomersController.history)
 
 module.exports = router;
