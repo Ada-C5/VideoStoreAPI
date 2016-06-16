5017a8d41f4816c1e89de2860afc8051f0e17ddc
Alysia Brown <alysiacbrown@gmail.com>
2016-06-16 14:12:18 -0700

    Added Customer.all model method.

---
+++ models/customers_model.js
@@ -0,0 +1,18 @@
+var app = require("../app");
+var db = app.get("db");
+
+var Customer = function(id) {
+  this.id = id;
+}
+
+Customer.all = function(callback) {
+  db.customers.find(function(error, accounts) {
+    if (error|| !customers) {
+      callback(error || new Error("Could not retrieve customers"), undefined);
+    } else {
+      callback(null, customers.map(function(customer) {
+        return new Customer(customer.id);
+      }))
+    };
+  })
+};
