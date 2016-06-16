e62b4ae65911f90429b76cccb3d74ea139c55879
Nadine Curry <nlcurry@gmail.com>
2016-06-16 15:32:12 -0700

    finished sortby and index function for customers

--- controllers/customers.js
+++ controllers/customers.js
@@ -9,24 +9,37 @@ var CustomersController = {
         next(err);
       } else {
         res.json(customers)
+      }
+    });
+  },
+
+  sortBy: function(req, res, next) {
+    console.log("bla",req.params.query, req.query.n, req.query.p)
+    var n = req.query.n
+    var p = req.query.p
+    var firstrow = n*(p-1)+1
+    var lastrow = n*p
+    Customer.sortByName([firstrow,lastrow],function(error, customers) {
+      if(error) {
+        var err = new Error("Error retrieving customer list:\n" + error.message);
+        err.status = 500;
+        next(err);
+      } else {
+        res.json(customers)
         // res.render("accounts/index", {
         //   accounts: accounts
         // });
       }
     });
-  },
-
-  function sortBy() {
-
-  },
-
-  function current() {
-
-  },
-
-  function history() {
-
   }
+
+  // function current() {
+
+  // },
+
+  // function history() {
+
+  // }
 }
 
-modules.exports = CustomersController
+module.exports = CustomersController
