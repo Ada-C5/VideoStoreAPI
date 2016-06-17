da60debe4863b1b847a7488a979d2b82fd5e8a72
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-06-17 14:48:24 -0700

    added n and p to queries involving configurable sorting

--- controllers/customers.js
+++ controllers/customers.js
@@ -15,11 +15,10 @@ var CustomerController = {
 
   sortName: function (req, res, next) {
     var n = req.query.n
-    var p = req.params.p
-    var limit = Number(p) * 10 //assumes 10 customer records per "page", unused
-    if (n === undefined) {
-    n = 10} // IMPERFECT - REVISIT AT REFACTOR
-    db.query("select * from customers order by name asc limit $1", [n], function(err, customerRecords){
+    var p = req.query.p
+    if (n === undefined) { n = 10 }
+    if ( p === undefined) { p = 1 }
+    db.query("select * from customers order by name asc limit $1 offset $2", [n, p], function(err, customerRecords){
       if(err) {
         var err = new Error("It's an error")
         next(err)
@@ -31,11 +30,10 @@ var CustomerController = {
 
   sortDate: function (req, res, next) {
     var n = req.query.n
-    var p = req.params.p
-    var limit = Number(p) * 10 //assumes 10 customer records per "page", unused
-    if (n === undefined) {
-    n = 10} // IMPERFECT - REVISIT AT REFACTOR
-    db.query("select * from customers order by registered_at asc limit $1", [n], function(err, customerRecords){
+    var p = req.query.p
+    if ( n === undefined) { n = 10 }
+    if ( p === undefined) { p = 1 }
+    db.query("select * from customers order by registered_at asc limit $1 offset $2", [n], function(err, customerRecords){
       if(err) {
         var err = new Error("It's an error")
         next(err)
@@ -48,10 +46,9 @@ var CustomerController = {
   sortPostalCode: function (req, res, next) {
     var n = req.query.n
     var p = req.params.p
-    var limit = Number(p) * 10 //assumes 10 customer records per "page", unused
-    if (n === undefined) {
-    n = 10} // IMPERFECT - REVISIT AT REFACTOR
-    db.query("select * from customers order by postal_code asc limit $1", [n], function(err, customerRecords){
+    if ( n === undefined) { n = 10 }
+    if ( p === undefined) { p = 1 }
+    db.query("select * from customers order by postal_code asc limit $1", [n, p], function(err, customerRecords){
       if(err) {
         var err = new Error("It's an error")
         next(err)
