c9bdf3f15a98ba18394b3d5a498fc30c2a463707
Nadine Curry <nlcurry@gmail.com>
2016-06-17 11:08:12 -0700

    working on current function

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,4 +1,5 @@
 var Customer = require("../models/customer")
+var Rental = require('../models/rental')
 
 var CustomersController = {
   index: function(req, res, next) {
@@ -29,15 +30,33 @@ var CustomersController = {
         res.json(customers)
       }
     });
+  },
+
+  current: function(req, res, next) {
+    var id = req.params.id
+    Rental.rentals(["checked_out", id], function(error, customers) {
+      if(error) {
+        var err = new Error("Error retrieving customer list:\n" + error.message);
+        err.status = 500;
+        next(err);
+      } else {
+        res.json(customers)
+      }
+    });
   }
 
-  // function current() {
-
-  // },
-
-  // function history() {
-
-  // }
+  history: function(req, res, next) {
+    var id = req.params.id
+    Rental.rentals(["returned", id], function(error, customers) {
+      if(error) {
+        var err = new Error("Error retrieving customer list:\n" + error.message);
+        err.status = 500;
+        next(err);
+      } else {
+        res.json(customers)
+      }
+    });
+  }
 }
 
 module.exports = CustomersController
