63a2dc805abf0c8cfac4f923511d747240b4e1a8
Adriana <dri19tcc@gmail.com>
2016-06-17 15:36:31 -0700

    Added in controller testing

---
+++ spec/controllers/rentals.spec.js
@@ -0,0 +1,38 @@
+var request = require("request")
+
+var baseUrl = "http://localhost:3000"
+
+
+describe("RentalsController", function() {
+  var url = function(endpoint) {
+    return baseUrl + "/rentals" + endpoint
+  }
+//
+  describe("#getRentals", function(done) {
+    it("returns a Success response", function(done) {
+      request.get(url("/"), function(error, response, body) {
+        expect(response.statusCode).toBe(200)
+        done()
+      })
+    })
+//
+    it("returns JSON", function(done) {
+      request.get(url("/"), function(error, response, body) {
+        expect(response.headers['content-type']).toContain('application/json')
+        done()
+      })
+    })
+//
+    it("should be an array of objects", function(done) {
+      request.get(url("/"), function(error, response, body) {
+        var data = JSON.parse(body)
+        expect(typeof data).toEqual('object')
+
+        for (var record of data) {
+          expect(Object.keys(record)).toEqual([ 'id', 'title', 'overview', 'release_date', 'inventory' ])
+        }
+        done()
+      })
+    })
+  })
+})
---
+++ spec/models/rental.spec.js
@@ -0,0 +1,93 @@
+var app = require('../../app')
+var db = app.get('db')
+
+var Rental = require('../../models/customer')
+
+// describe('Rental', function () {
+//   var customer1 = "Acton Gilliam"
+//   var registered_at1 = "Thu, 26 Feb 2015 20:00:53 -0800"
+//   var address1 = 'Ap #508-8214 Senectus Av.'
+//   var city1 = "Portland"
+//   var state1 = "Oregon"
+//   var postal_code1 = "62594"
+//   var phone1 = "(903) 973-1984"
+//   var account_credit1 = "48.64"
+//
+//   afterEach(function () {
+//     db.end()
+//   })
+//
+//   describe('#all', function () {
+//     it('should return all customers', function (done) {
+//       Rental.all(function (error, customers) {
+//         expect(customers.length).toEqual(200)
+//       })
+//       done()
+//     })
+//   })
+//
+//   describe('#find', function () {
+//     it('should return correct customer', function (done) {
+//        Rental.find(customer1, function (error, customer) {
+//         expect(customer.name).toEqual(customer1)
+//         expect(customer.registered_at).toEqual(registered_at1)
+//         expect(customer.address).toEqual(address1)
+//         expect(customer.city).toEqual(city1)
+//         expect(customer.state).toEqual(state1)
+//         expect(customer.postal_code).toEqual(postal_code1)
+//         expect(customer.phone).toEqual(phone1)
+//         expect(customer.account_credit).toEqual(account_credit1)
+//         expect(error).toBeNull
+//         done()
+//       })
+//     })
+//
+//     it('should throw an error if the customer DNE', function (done) {
+//       Rental.find("person", function(error, customer) {
+//         expect(error.message).toEqual("Rental not found")
+//       })
+//     done()
+//     })
+//   })
+//
+//   describe('#sort', function () {
+//     it('should sort customer by name', function(done) {
+//       Rental.sort("name", 1, 3, function(error, customer_array) {
+//         expect(customer_array.length).toEqual(1)
+//         expect(customer_array[0].name).toEqual(customer1)
+//         expect(customer_array[0].registered_at).toEqual(registered_at1)
+//         expect(customer_array[0].address).toEqual(address1)
+//         expect(customer_array[0].city).toEqual(city1)
+//         expect(customer_array[0].state).toEqual(state1)
+//         expect(customer_array[0].postal_code).toEqual(postal_code1)
+//         expect(customer_array[0].phone).toEqual(phone1)
+//         expect(customer_array[0].account_credit).toEqual(account_credit1)
+//         expect(error).toBeNull
+//       })
+//       done()
+//     })
+//
+//     it('should throw an error if the customer DNE', function (done) {
+//       Rental.sort("abcd", 1, 3, function(error, customer_array2) {
+//         expect(error.message).toEqual("Could not retrieve customers")
+//       })
+//     done()
+//     })
+//   })
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
