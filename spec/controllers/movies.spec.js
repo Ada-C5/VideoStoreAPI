105ddba935169d57bf3b643adf37f253086a8954
Valerie <val.conklin@gmail.com>
2016-06-17 14:34:20 -0700

    Trying to get test script working

--- spec/controllers/movies.spec.js
+++ spec/controllers/movies.spec.js
@@ -1,5 +1,15 @@
 var request = require('request');
+var base_url = "http://localhost:3000/"
+var app = require('../../app.js');
 
 describe("Endpoints under /movies", function() {
-  
+  it('responds with a 200 status code', function (done) {
+    request.get(base_url, function(error, response, body) {
+      expect(response.statusCode).toEqual(200)
+      done()
+    })
+  });
+
+
+
 });
