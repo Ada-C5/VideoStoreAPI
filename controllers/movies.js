8210c54508deb712405d0a6923e87fc6683484da
Nadine Curry <nlcurry@gmail.com>
2016-06-17 09:45:05 -0700

    added sortby method

--- controllers/movies.js
+++ controllers/movies.js
@@ -11,11 +11,24 @@ var MoviesController = {
         res.json(movies)
       }
     });
-  }
+  },
 
-  // function sortBy () {
-
-  // },
+  sortBy: function(req, res, next) {
+  var type = req.params.query
+  var n = req.query.n
+  var p = req.query.p
+  var firstrow = n*(p-1)+1
+  var lastrow = n*p
+  console.log("bla",type,firstrow,lastrow)
+  Movie.sortBy([type,firstrow,lastrow],function(error, movies) {
+    if(error) {
+      var err = new Error("Error retrieving customer list:\n" + error.message);
+      err.status = 500;
+      next(err);
+    } else {
+      res.json(movies)
+    }
+  })}
 
   // function current () {
 
