c9a803dbad8f6acc5f77bdbed122f1b6d83ba070
Ania Gonzalez <aniagr@gmail.com>
2016-06-17 10:51:07 -0700

    got Customers.all working

--- models/customer.js
+++ models/customer.js
@@ -2,22 +2,22 @@ var app = require("../app");
 var db = app.get("db");
 
 
-var Customer = function(id) {
-  this.id = id
-  this.name = name
-  this.registered_at = registered_at
-  this.address = address
-  this.city = city
-  this.state = state
-  this.postal_code = postal_code
-  this.phone = phone
-  this.account_credit = account_credit
+var Customer = function(customer) {
+  this.id = customer.id
+  this.name = customer.name
+  this.registered_at = customer.registered_at
+  this.address = customer.address
+  this.city = customer.city
+  this.state = customer.state
+  this.postal_code = customer.postal_code
+  this.phone = customer.phone
+  this.account_credit = customer.account_credit
 }
 
 // takes on parameter(callback)-then run db.accounts.find
 Customer.all = function(callback) {
   // then run db.accounts.find(no specific id or column - just another callback)
-  db.customers.find('*', function(error, customers) {
+  db.customers.find(function(error, customers) {
     if(error || !customers) {
       // handling any error
       callback(error || new Error("Could not retrieve customers"), undefined);
@@ -25,7 +25,7 @@ Customer.all = function(callback) {
       // saying there was no error, accounts is an array and we map it
       callback(null, customers.map(function(customer) {
         // and return to a new instance of the account with id
-        return new Customer(customer.id);
+        return new Customer(customer);
       }));
     };
   });
