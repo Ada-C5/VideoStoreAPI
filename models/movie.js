55590cb837ed302449a58efe32f18d76f99b606b
Nadine Curry <nlcurry@gmail.com>
2016-06-17 09:45:29 -0700

    added sortby method

--- models/movie.js
+++ models/movie.js
@@ -5,6 +5,7 @@ var db = app.get("db");
 var Movie = function(movie) {
   this.id = movie.id;
   this.title = movie.title;
+  this.release = movie.release_date;
 };
 
 
@@ -12,6 +13,19 @@ Movie.all = function(callback) {
   db.run("SELECT * FROM movies;", function(error, movies) {
     if(error || !movies) {
       callback(error || new Error("Could not retrieve movies"), undefined);
+    } else {
+      callback(null, movies.map(function(movie) {
+        return new Movie(movie);
+      }));
+    }
+  });
+};
+
+Movie.sortBy = function(input, callback){
+  var order = input.shift()
+  db.run("Select * From (Select Row_Number() Over (Order By " + order + ") As RowNum, *From movies) movies Where RowNum BETWEEN $1 AND $2;",input, function(error, movies) {
+    if(error || !movies) {
+      callback(error || new Error("Could not retrieve Movies"), undefined);
     } else {
       callback(null, movies.map(function(movie) {
         return new Movie(movie);
