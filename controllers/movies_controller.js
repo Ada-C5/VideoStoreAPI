d070933378943a19e8cbcad2b598335b2dd8361f
Suzanne Harrison <harrison_family@hotmail.com>
2016-06-17 16:27:32 -0700

    movie/title/current route enabled. starting to work on movie controller.

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -1,4 +1,6 @@
 var Movie = require("../models/movie");
+var Customer = require("../models/customer");
+var Rental = require("../models/rental");
 
 var MoviesController = {
   listMovies: function(req, res, next) {
@@ -15,29 +17,48 @@ var MoviesController = {
       }
     });
   },
-sortBy: function(req, res, next) {
-  var options = {
-    order: req.params.field,
-    limit: req.query.n,
-    offset: req.query.p
-  }
 
-  Movie.sortBy(options, function(error, movies) {
-    if(error) {
-      var err = new Error("Error retrieving sorted movie list:\n" + error.message);
-      err.status = 500;
-      next(err);
-    } else {
-      res.json(movies)
-      // var locals = { movies: movies }
-      // res.render("movies/index", locals);
+  sortBy: function(req, res, next) {
+    var options = {
+      order: req.params.field,
+      limit: req.query.n,
+      offset: req.query.p
     }
-  });
+
+    Movie.sortBy(options, function(error, movies) {
+      if(error) {
+        var err = new Error("Error retrieving sorted movie list:\n" + error.message);
+        err.status = 500;
+        next(err);
+      } else {
+        res.json(movies)
+        // var locals = { movies: movies }
+        // res.render("movies/index", locals);
+      }
+    });
  },
 
-  // current: function(req, res, next)  {
-  //
-  // },
+  current: function(req, res, next)  {
+    var movie = req.params.title;
+
+    Movie.find(movie, function(error, search_movie)
+      if(error) {
+          var err = new Error("No such movie");
+          err.status = 404;
+          next(err);
+        } else {
+          Rentals.find(function(error, movie) {
+            res.render("rentals/show", {
+              movie: {
+                id: movie.id,
+              }
+            });
+          });
+        }
+      });
+  )
+
+  },
   //
   // sortByHistory: function(req, res, next)  {
   //
