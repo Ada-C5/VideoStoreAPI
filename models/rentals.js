f74c963fccf3ea75dc5a4f68c136d797d277b862
Jillian Boshart <Jilliliz@gmail.com>
2016-06-20 10:53:24 -0700

    Added more seed data inorder to test sorting function of rentals history/current. Added SQL query for ordering by checkout date.

--- models/rentals.js
+++ models/rentals.js
@@ -17,7 +17,7 @@ Rentals.find_current = function(customer_id, callback) {
 )}
 
 Rentals.find_history = function(customer_id, callback) {
-  db.rentals.where("customer_id=$1 AND checkin_date IS NOT NULL", [customer_id], function(error, rentals) {
+  db.rentals.where("customer_id=$1 AND checkin_date IS NOT NULL ORDER BY checkout_date", [customer_id], function(error, rentals) {
     if(error || !rentals) {
       callback(error || new Error("Rentals not found"), undefined);
     } else {
@@ -25,34 +25,6 @@ Rentals.find_history = function(customer_id, callback) {
     }
   }
 )}
-// Rentals.all = function(callback) {
-//   db.rentals.find(function(error, rentals) {
-//     if(error || !rentals) {
-//       callback(error || new Error("Could not retrieve rentals"), undefined);
-//     } else {
-//       callback(null, Rentals.map(function(rental) {
-//         return new Rentals(rental);
-//       }));
-//     }
-//   });
-// };
-//
-//
-// Rentals.sort = function(column, p, n, callback) {
-//  db.rentals.find({}, {
-//     order: column,
-//     limit: n,
-//     offset: p
-//   }, function(error, rentals) {
-//     if (error || !rentals) {
-//       callback(error || new Error("Could not retrieve rentals"), undefined)
-//     } else {
-//       callback(null, Rentals.map(function(rental) {
-//         return rental
-//       }));
-//     }
-//   });
-// }
 
 
 module.exports = Rentals;
