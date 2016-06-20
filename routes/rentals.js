da6a0f292ecf10a2180e15451a477a5afdd5a5c1
Nadine Curry <nlcurry@gmail.com>
2016-06-20 13:48:34 -0700

    uncommented and change function name for rental route

--- routes/rentals.js
+++ routes/rentals.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var RentalsController = require('../controllers/rentals')
 
 router.get('/overdue', RentalsController.overdue)
+router.get('/:movie/customers', RentalsController.currentlyCheckedOut) // do we need to pass stuff in?
 router.get('/:movie', RentalsController.lookupMovie)
-// router.get('/:movie/customers', RentalsController.sortBy) // do we need to pass stuff in?
 // router.post('/:movie/check-out', RentalsController.checkOut) // requires json data customer_id and movie title
 // router.post('/:movie/return', RentalsController.return) // requires json data customer_id and movie title
 
