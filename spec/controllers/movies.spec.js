9aee2be0bd4922bf9eedf655afd4201f5a5b18d1
Anna Wilson <annie.marie.mason@gmail.com>
2016-06-20 15:04:15 -0700

    tests for /movies route

--- spec/controllers/movies.spec.js
+++ spec/controllers/movies.spec.js
@@ -1,5 +1,26 @@
-var request = require('request');
+var request = require('request')
+var base_url = "http://localhost:3000/movies"
 
-describe("Endpoints under /movies", function() {
-  
+describe("Endpoint at /movies", function () {
+  it('responds with a 200 status code', function (done) {
+    request.get(base_url, function(error, response, body) {
+      expect(response.statusCode).toEqual(200)
+      done()
+    })
+  })
+
+  it("returns JSON", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.headers['content-type']).toContain('application/json')
+        done()
+      })
+    })
+
+    it('has the right values', function(done) {
+      request.get(base_url, function(error, response, body) {
+        var data = JSON.parse(body)
+        expect(data['0'].title).toEqual('Psycho')
+        done()
+      })
+    })
 })
