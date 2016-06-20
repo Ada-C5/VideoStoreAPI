4a3e3aa173f771715c69eafd21ecd280abc925c6
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-06-20 16:16:42 -0700

    Added .json documentation endpoint, successful.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var documentation = require('../lib/documentation')
-
+var himalaya = require('himalaya');
+var html = require('fs').readFileSync('README.md');
+var json = himalaya.parse(html); //actually md. close enough.
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,7 +14,11 @@ router.get('/zomg', function(req, res, next) {
 });
 
 router.get('/api/docs', function(req, res, next) {
-  res.status(200).json({whatevs: 'whatevs!!!'})
+  res.render('../views/index')
+});
+
+router.get('/api/docs.json', function(req, res, next) {
+  res.json(json)
 });
 
 module.exports = router;
