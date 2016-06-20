ef5e344af058b4e958c6594c7f97529b7e082e37
Suzanne Harrison <harrison_family@hotmail.com>
2016-06-20 14:52:03 -0700

    saving routes uncommented now.

--- routes/movies.js
+++ routes/movies.js
@@ -5,6 +5,6 @@ var MoviesController = require('../controllers/movies_controller.js');
 router.get('/', MoviesController.listMovies)
 router.get('/sort/:field', MoviesController.sortBy)
 router.get('/:title/current', MoviesController.current)
-// router.get('/:title/history/sort/:by', MoviesController.sortByHistory)
+router.get('/:title/history/sort/:field', MoviesController.history)
 
 module.exports = router;
