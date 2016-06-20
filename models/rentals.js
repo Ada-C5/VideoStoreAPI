23a8b4daf41b40c16677fc19d93babf374ce8a04
Jillian Boshart <Jilliliz@gmail.com>
2016-06-20 15:26:18 -0700

    video_current now returns the customers who currently have a particular video checked out.

--- models/rentals.js
+++ models/rentals.js
@@ -7,7 +7,7 @@ var Rentals = function(customer_id) {
 };
 
 Rentals.find_current = function(customer_id, callback) {
-  db.rentals.where("customer_id=$1 AND checkin_date is null", [customer_id], function(error, rentals) {
+  db.rentals.where("customer_id=$1 AND checkin_date=null", [customer_id], function(error, rentals) {
     if(error || !rentals) {
       callback(error || new Error("Rentals not found"), undefined);
     } else {
@@ -27,15 +27,54 @@ Rentals.find_history = function(customer_id, callback) {
 )}
 
 Rentals.video_current = function(title, callback) {
-  var video = db.videos.findOne(title)
-  console.log(video)
-  db.rentals.where(video.id, function(error, rentals) {
-    if(error || !rentals) {
+  db.videos.findOne({title: title}, function(error, videos) {
+
+    if (error || !videos) {
       callback(error || new Error("Rentals not found"), undefined);
     } else {
-      callback(null, rentals);
+
+      var video_id = videos.id
+      var cust = []
+      db.rentals.find({video_id: video_id, checkin_date: null}, function(error, rentals) {
+        if(error || !rentals) {
+          callback(error || new Error("Rentals not found"), undefined);
+        } else {
+
+          for (var rental of rentals) {
+            db.customers.findOne({id: rental.customer_id}, function(error, customer) {
+              cust.push(customer)
+              if (cust.length === rentals.length) {
+                callback(null, cust);
+              }
+            })
+          }
+        }
+      })
     }
-  }
-)}
+  })
+}
+
+
+          // for (var rental of rentals) {
+          //   var customer_id = rental.customer_id
+          //   db.customers.find({id: customer_id}, function(error, customer) {
+          //     if(error || !customer) {
+          //       callback(error || new Error("Customer not found"), undefined);
+          //     } else {
+          //       // console.log(customer)
+          //       cust.push(customer[0])
+          //       // console.log(cust)
+          //       return cust
+          //     }
+          //   })
+          //   console.log(cust)
+
+//           }
+//         })
+//       }
+//     }
+//   })
+// }
+
 
 module.exports = Rentals;
