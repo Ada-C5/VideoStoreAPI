440cae3638b6371c6df3bb3170e14bcc50341f17
Jessica <jessica.weeber@gmail.com>
2016-06-20 10:33:36 -0700

    added temporary seed data to test customer routes for current checked out movies and rental history. added route for this and isntabul test path

--- routes/rentals.js
+++ routes/rentals.js
@@ -4,6 +4,6 @@ var Controller = require('../controllers/rentals')
 
 router.get('/', Controller.getRentals)
 router.get('/:title', Controller.getRentalsShow)
-// router.get('/:title/customers', Controller.getRentalStatus)
+router.get('/customers/:id/current', Controller.getRentalsCurrent)
 
 module.exports = router
