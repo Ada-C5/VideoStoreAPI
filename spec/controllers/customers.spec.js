f55f4d0a06696f5f6dd9adefb0ee684ffecaab3d
Alysia Brown <alysiacbrown@gmail.com>
2016-06-22 13:49:12 -0700

    Added basic customer controller tests.

--- spec/controllers/customers.spec.js
+++ spec/controllers/customers.spec.js
@@ -4,9 +4,30 @@ var route = "customers/"
 // var app = require('../../app.js');
 
 describe("Endpoints under /customers", function() {
+
   it('responds with a 200 status code', function (done) {
     request.get(base_url + route, function(error, response, body) {
       expect(response.statusCode).toEqual(200)
+      done()
+    })
+  });
+
+  it("should be json", function(done) {
+  request.get(base_url + route, function(error, response, body) {
+    expect(response.headers['content-type']).toContain('application/json')
+    done()
+  })
+  });
+
+  it("should be an array of objects", function(done) {
+      request.get(base_url + route, function(error, response, body) {
+        var data = JSON.parse(body)
+        expect(typeof data).toEqual('object')
+
+        for (var record of data) {
+          expect(Object.keys(record)).toEqual(['id'])
+        }
+
       done()
     })
   });
