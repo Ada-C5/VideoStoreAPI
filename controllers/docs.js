4c2b109e17a0f6696bbb5459667cb05e97a35ae4
Leah <leahnpetersen@gmail.com>
2016-06-23 10:43:23 -0700

    added json file, trying to find the best way to print it out in a view

--- controllers/docs.js
+++ controllers/docs.js
@@ -1,10 +1,14 @@
 // var Customer = require("../models/customers_model");
 // var Rental = require("../models/rentals_model");
+		var json = require('../public/docs.json');
 
 
 var DocsController = {
 	getDocs: function(req, res) {
-		res.render('docs', { title: 'The Docs' });
+		res.render('docs', { 
+			title: 'The Docs',
+			list: JSON.stringify(json) 
+		});
 	}
 	// getDocs: function(req, res) {
 	// 	Movie.all(function(error, docs) {
