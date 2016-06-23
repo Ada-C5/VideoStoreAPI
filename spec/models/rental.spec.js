d1f14ac01a4d8541863e626a9d3ee2803d18f967
Nadine Curry <nlcurry@gmail.com>
2016-06-23 13:51:54 -0700

    added currentcheckedout function tests

--- spec/models/rental.spec.js
+++ spec/models/rental.spec.js
@@ -1,7 +1,7 @@
 var app = require('../../app')
 var db = app.get('db')
 
-var Movie = require('../../models/rental')
+var Rental = require('../../models/rental')
 
 describe('Rental', function () {
 
@@ -9,9 +9,37 @@ describe('Rental', function () {
     db.end()
   })
 
-  describe('', function () {
+  describe('.currentCheckedOut', function () {
+    // input should be [order, firstrow, lastrow]
+    it('should return an array', function(done) {
+      Rental.currentCheckedOut([55,'checked_out'], function(error,rentals){
+        expect(rentals).toEqual(jasmine.any(Array))
+        done()
+      })
+    })
 
-
+    it('be a certain size', function(done) {
+      Rental.currentCheckedOut([4,'checked_out'], function(error,rentals){
+        expect(rentals.length).toEqual(2)
+        done()
+      })
+    })
   })
 
+  describe('.currentCheckedOut', function () {
+    // input should be [order, firstrow, lastrow]
+    it('should return an array', function(done) {
+      Rental.currentCheckedOut([55,'checked_out'], function(error,rentals){
+        expect(rentals).toEqual(jasmine.any(Array))
+        done()
+      })
+    })
+
+    it('be a certain size', function(done) {
+      Rental.currentCheckedOut([4,'checked_out'], function(error,rentals){
+        expect(rentals.length).toEqual(2)
+        done()
+      })
+    })
+  })
 })
