7c20efbf2ea8b04bd71401325ce2a90356b0d171
Nadine Curry <nlcurry@gmail.com>
2016-06-23 10:14:04 -0700

    added tests for customers.all

--- spec/models/customer.spec.js
+++ spec/models/customer.spec.js
@@ -1,7 +1,7 @@
 var app = require('../../app')
 var db = app.get('db')
 
-var Movie = require('../../models/customer')
+var Customer = require('../../models/customer')
 
 describe('Customer', function () {
   beforeEach(function(){
@@ -12,11 +12,24 @@ describe('Customer', function () {
     db.end()
   })
 
-  describe('Customer.all', function () {
+  describe('.all', function () {
     it('should return an array', function(done) {
-
+      Customer.all(function(error,customers){
+        expect(customers).toEqual(jasmine.any(Array))
+        done()
       })
+
     })
+
+    it('be at least certain size', function(done) {
+      Customer.all(function(error,customers){
+        expect(customers.length).toBeGreaterThan(199)
+        done()
+      })
+
+    })
+
   })
+})
 
 
