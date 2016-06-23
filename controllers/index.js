3501756417e126c1e73d78d2f463627f69d0fe79
Valerie <val.conklin@gmail.com>
2016-06-23 11:29:58 -0700

    Currently working on api HTML version

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,13 @@
 var docs = require('../docs.json');
+var docs_view = require('../views/docs.ejs')
 
 var IndexController = {
   index: function(req, res, next) {
     res.json('It works!!');
+  },
+
+  docsHTML: function(req, res, next) {
+    res.render(docs_view);
   },
 
   docsJSON: function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ var IndexController = require('../controllers/index.js');
 
 router.get('/', IndexController.index);
 
-/* test GET page. */
+router.get('/api/docs', IndexController.docsHTML);
+
+router.get('/api/docs.json', IndexController.docsJSON);
+
+/**** just a test ****/
 router.get('/zomg', function(req, res, next) {
   res.status(200).json({whatevs: 'whatevs!!!'});
 });
-
-router.get('/api/docs.json', IndexController.docsJSON)
 
 module.exports = router;
