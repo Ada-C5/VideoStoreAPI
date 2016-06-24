54f7921fc0807f954d7b54045fdddf5048681d33
sophia <sophiabaldonado@gmail.com>
2016-06-24 09:57:15 -0700

    Movies Controller tests;

--- spec/controllers/movies.spec.js
+++ spec/controllers/movies.spec.js
@@ -23,11 +23,58 @@ describe("Endpoints under /movies", function() {
     })
   })
 
-  describe('/movies/sort/')
-  it('returns json', function (done) {
-    request.get(base_url, function(error, response, body) {
-      expect(response.headers['content-type']).toContain('application/json')
-      done()
+  describe('/movies/sort/', function () {
+    var goodSort = 'sort/release_date?n=5&p=1'
+    var badSort = 'sort/year?n=5&p1'
+
+    it('responds with 200 for a good request', function (done) {
+      request.get(base_url + goodSort, function(error, response, body) {
+        expect(response.statusCode).toEqual(200)
+        done()
+      })
     })
+
+    it('returns json', function (done) {
+      request.get(base_url + goodSort, function(error, response, body) {
+        expect(response.headers['content-type']).toContain('application/json')
+        done()
+      })
+    })
+
+    it('responds with 500 for a bad request', function (done) {
+      request.get(base_url + badSort, function(error, response, body) {
+        expect(response.statusCode).toEqual(500)
+        done()
+      })
+    })
+
   })
+
+  describe('/movies/:title/current', function () {
+    var goodCurrent = 'Psycho/current'
+    var badCurrent = 'Young%20Frankenstein/current'
+
+    it('responds with 200 for a good request', function (done) {
+      request.get(base_url + goodCurrent, function(error, response, body) {
+        expect(response.statusCode).toEqual(200)
+        done()
+      })
+    })
+
+    it('returns json', function (done) {
+      request.get(base_url + goodCurrent, function(error, response, body) {
+        expect(response.headers['content-type']).toContain('application/json')
+        done()
+      })
+    })
+
+    it('responds with empty array for bad request', function (done) {
+      request.get(base_url + badCurrent, function(error, response, body) {
+        expect(response).toEqual([])
+        done()
+      })
+    })
+
+  })
+
 })
