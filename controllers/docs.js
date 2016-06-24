965f029aa7239b776437635e71c0f877e1d40cb1
Leah <leahnpetersen@gmail.com>
2016-06-23 20:57:35 -0700

    added html formatting for docs view to iterate through json file and print ul for each endpoint

--- controllers/docs.js
+++ controllers/docs.js
@@ -1,25 +1,15 @@
 // var Customer = require("../models/customers_model");
 // var Rental = require("../models/rentals_model");
-		var json = require('../public/docs.json');
+var docs = require('../public/docs.json');
 
 
 var DocsController = {
 	getDocs: function(req, res) {
 		res.render('docs', { 
 			title: 'The Docs',
-			list: JSON.stringify(json) 
+			docs: docs
 		});
 	}
-	// getDocs: function(req, res) {
-	// 	Movie.all(function(error, docs) {
-	// 		if(error) {
-	// 			var err = new Error("Error retrieving movie list;\n" + error.message);
-	// 			err.status = 500;
-	// 		} else {
-	// 			res.json(docs)
-	// 		}
-	// 	})
-	// }
 }
 
 module.exports = DocsController
