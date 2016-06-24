955dace7715984d99f573a5430efd0fda4bde0d2
Alysia Brown <alysiacbrown@gmail.com>
2016-06-24 11:29:42 -0700

    Controller tests finished.

--- spec/controllers/customers.spec.js
+++ spec/controllers/customers.spec.js
@@ -23,14 +23,93 @@ describe("Endpoints under /customers", function() {
         var data = JSON.parse(body)
         expect(typeof data).toEqual('object')
 
-        for (var record of data) {
-          expect(Object.keys(record)).toEqual(['id'])
-        }
+          expect(Object.keys(data[0])).toEqual([ 'id' ])
 
       done()
     })
   });
+});
 
+describe("Endpoints under /customers/sort/name", function() {
 
+  it('responds with a 200 status code', function (done) {
+    request.get(base_url + route + "/sort/name", function(error, response, body) {
+      expect(response.statusCode).toEqual(200)
+      done()
+    })
+  });
 
+  it("should be json", function(done) {
+  request.get(base_url + route + "/sort/name", function(error, response, body) {
+    expect(response.headers['content-type']).toContain('application/json')
+    done()
+  })
+  });
+
+  it("should be an array of objects", function(done) {
+      request.get(base_url + route + "/sort/name", function(error, response, body) {
+        var data = JSON.parse(body)
+        expect(typeof data).toEqual('object')
+
+          expect(Object.keys(data[0])).toEqual([ 'id', 'name', 'registered_at', 'address', 'city', 'state', 'postal_code', 'phone', 'account_credit' ])
+
+      done()
+    })
+  });
+});
+
+describe("Endpoints under /customers/:id/current", function() {
+
+  it('responds with a 200 status code', function (done) {
+    request.get(base_url + route + "/5/current", function(error, response, body) {
+      expect(response.statusCode).toEqual(200)
+      done()
+    })
+  });
+
+  it("should be json", function(done) {
+  request.get(base_url + route + "/5/current", function(error, response, body) {
+    expect(response.headers['content-type']).toContain('application/json')
+    done()
+  })
+  });
+
+  it("should be an array of objects", function(done) {
+      request.get(base_url + route + "/5/current", function(error, response, body) {
+        var data = JSON.parse(body)
+        expect(typeof data).toEqual('object')
+
+          expect(Object.keys(data[0])).toEqual([ 'id', 'movie_id', 'customer_id', 'created_date', 'due_date', 'returned', 'returned_date' ])
+
+      done()
+    })
+  });
+});
+
+describe("Endpoints under /customers/:id/history", function() {
+
+  it('responds with a 200 status code', function (done) {
+    request.get(base_url + route + "/5/history", function(error, response, body) {
+      expect(response.statusCode).toEqual(200)
+      done()
+    })
+  });
+
+  it("should be json", function(done) {
+  request.get(base_url + route + "/5/history", function(error, response, body) {
+    expect(response.headers['content-type']).toContain('application/json')
+    done()
+  })
+  });
+
+  it("should be an array of objects", function(done) {
+      request.get(base_url + route + "/5/history", function(error, response, body) {
+        var data = JSON.parse(body)
+        expect(typeof data).toEqual('object')
+
+          expect(Object.keys(data[0])).toEqual([ 'customer_id', 'created_date', 'movie_id', 'returned_date' ])
+
+      done()
+    })
+  });
 });
--- spec/controllers/movies.spec.js
+++ spec/controllers/movies.spec.js
@@ -4,10 +4,118 @@ var route = "movies/"
 
 
 describe("Endpoints under /movies", function() {
+
   it('responds with a 200 status code', function (done) {
     request.get(base_url + route, function(error, response, body) {
       expect(response.statusCode).toEqual(200)
       done()
     })
   });
+
+  it("should be json", function(done) {
+  request.get(base_url + route, function(error, response, body) {
+    expect(response.headers['content-type']).toContain('application/json')
+    done()
+  })
+  });
+
+  it("should be an array of objects", function(done) {
+      request.get(base_url + route, function(error, response, body) {
+        var data = JSON.parse(body)
+        expect(typeof data).toEqual('object')
+
+          expect(Object.keys(data[0])).toEqual([ 'title' ])
+
+      done()
+    })
+  });
 });
+
+
+describe("Endpoints under /movies/sort/:column", function() {
+
+  it('responds with a 200 status code', function (done) {
+    request.get(base_url + route + '/sort/title', function(error, response, body) {
+      expect(response.statusCode).toEqual(200)
+      done()
+    })
+  });
+
+  it("should be json", function(done) {
+  request.get(base_url + route + '/sort/title', function(error, response, body) {
+    expect(response.headers['content-type']).toContain('application/json')
+    done()
+  })
+  });
+
+  it("should be an array of objects", function(done) {
+      request.get(base_url + route + '/sort/title', function(error, response, body) {
+        var data = JSON.parse(body)
+        expect(typeof data).toEqual('object')
+
+          expect(Object.keys(data[0])).toEqual([ 'id', 'title', 'overview', 'release_date', 'inventory' ])
+
+      done()
+    })
+  });
+
+});
+
+describe("Endpoints under /movies/:title/current", function() {
+
+  it('responds with a 200 status code', function (done) {
+    request.get(base_url + route + "/Jaws/current", function(error, response, body) {
+      expect(response.statusCode).toEqual(200)
+      done()
+    })
+  });
+
+  it("should be json", function(done) {
+  request.get(base_url + route + "/Jaws/current", function(error, response, body) {
+    expect(response.headers['content-type']).toContain('application/json')
+    done()
+  })
+  });
+
+  it("should be an array of objects", function(done) {
+      request.get(base_url + route + "/Jaws/current", function(error, response, body) {
+        var data = JSON.parse(body)
+        expect(typeof data).toEqual('object')
+
+          expect(Object.keys(data[0])).toEqual([ 'id', 'name', 'registered_at', 'address', 'city', 'state', 'postal_code', 'phone', 'account_credit' ])
+
+      done()
+    })
+  });
+});
+
+// ******** history path tests are causing server timeouts. wtf?? ********
+// describe("Endpoints under /movies/:id/history", function() {
+//
+//   it('responds with a 200 status code', function (done) {
+//     request.get(base_url + route + "/5/history", function(error, response, body) {
+//       expect(response.statusCode).toEqual(200)
+//       done()
+//     })
+//   });
+//
+//   it("should be json", function(done) {
+//   request.get(base_url + route + "/5/history", function(error, response, body) {
+//     expect(response.headers['content-type']).toContain('application/json')
+//     done()
+//   })
+//   });
+//
+//   it("should be an array of objects", function(done) {
+//       request.get(base_url + route + "/5/history", function(error, response, body) {
+//         var data = JSON.parse(body)
+//         expect(typeof data).toEqual('object')
+//
+//         for (var record of data) {
+//           expect(Object.keys(data[0])).toEqual(['id'])
+//         }
+//
+//       done()
+//     })
+//   });
+// });
