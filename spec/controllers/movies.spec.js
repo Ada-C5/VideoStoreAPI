13877176c798863d34a36fa3e350b2901060e2b8
Nadine Curry <nlcurry@gmail.com>
2016-06-24 09:48:13 -0700

    got rid of console.log in customers and added 2 function tests in rentals and 1 in movies

--- spec/controllers/movies.spec.js
+++ spec/controllers/movies.spec.js
@@ -1,11 +1,22 @@
-var request = require('request');
-var base_url = "http://localhost:3000/movies/"
+var request = require('request')
+var app = require("../../app.js")
+var base_url = "http://localhost:3000/movies"
 
-describe("Endpoints under /movies", function() {
-  it('responds with a 200 status code', function (done) {
-    request.get(base_url, function(error, response, body) {
-      expect(response.statusCode).toEqual(200)
-      done()
+describe("RentalsController", function(){
+
+  describe("#index", function(done){
+    it("returns a success response", function(done){
+      request.get(base_url, function(error, response, body){
+        expect(response.statusCode).toBe(200)
+        done()
+      })
+    })
+
+    it("returns json", function(done){
+      request.get(base_url, function(error, response, body){
+        expect(response.headers['content-type']).toContain('application/json')
+        done()
+      })
     })
   })
 })
