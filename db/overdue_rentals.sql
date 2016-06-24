e6a0e2e7757851d00278d8ecef212dfa56966ad8
Adriana <dri19tcc@gmail.com>
2016-06-24 11:00:43 -0700

    Finished function to return customers that have late data

--- db/overdue_rentals.sql
+++ db/overdue_rentals.sql
@@ -1,6 +1,6 @@
 
-SELECT rentals.status, rentals.customer_id, customers.*
+SELECT rentals.status, rentals.customer_id, rentals.return_date, customers.*
 FROM rentals
 INNER JOIN customers
 ON customers.id=rentals.customer_id
-WHERE rentals.status=true;
+WHERE rentals.status=true AND to_date(return_date, 'Dy Mon DD YYYY')<current_date;
--- models/rental.js
+++ models/rental.js
@@ -118,18 +118,6 @@ Rental.overdueRental = function (callback) {
       return new Customer(customer)
     }))
   })
-  // db.rentals.find({ status: true }, function (error, rentals) {
-  //   callback(null, rentals.map (function (rental) {
-  //     var today = new Date()
-  //     var returnDate = rental.return_date
-  //     if (Date.parse(returnDate) < today) {
-  //       db.customers.find({id: rental.customer_id}, function (err, customer) {
-  //         console.log(customer[0]);
-  //       })
-  //     }
-  //     return new Customer(customer)
-  //   }))
-  // })
 }
 
 Rental.removeInventory = function (movie) {
