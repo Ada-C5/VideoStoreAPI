417de7e419df673895f2baf707bac743ae36633a
Jessica <jessica.weeber@gmail.com>
2016-06-24 14:02:17 -0700

    fixing sql for movie rental history showing customers

--- db/customer_rental_history.sql
+++ db/customer_rental_history.sql
@@ -1,18 +1,8 @@
-
 SELECT movies.id, rentals.movie_id, rentals.customer_id, rentals.status, customers.*
 FROM rentals
 INNER JOIN movies
 ON movies.id=rentals.movie_id
 INNER JOIN customers
 ON rentals.customer_id=customers.id
-WHERE movies.title=$1 AND rentals.status=false
+WHERE movies.title=$1
 ORDER BY customers.name;
-
--- SELECT rentals.movie_id, rentals.customer_id, rentals.status, rentals.checkout_date, customers.*, movies.*
--- FROM rentals
--- INNER JOIN movies
--- ON movies.id=rentals.movie_id
--- INNER JOIN customers
--- ON rentals.customer_id=customers.id
--- WHERE movies.title=$1 AND rentals.status=false
--- ORDER BY to_date(rentals.checkout_date, 'Dy Mon DD YYYY');
